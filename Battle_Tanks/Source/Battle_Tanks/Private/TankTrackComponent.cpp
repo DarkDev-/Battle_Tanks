676960ef577e8c71d2818c6d34a1e1fa53e50368
Anton Edvardsson <nufnad@gmail.com>
2018-06-26 21:46:38 +0200

    Add sideway friction

--- Battle_Tanks/Source/Battle_Tanks/Private/TankTrackComponent.cpp
+++ Battle_Tanks/Source/Battle_Tanks/Private/TankTrackComponent.cpp
@@ -3,13 +3,39 @@
 #include "TankTrackComponent.h"
 
 
+UTankTrackComponent::UTankTrackComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrackComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	root = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+}
+
+void UTankTrackComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!ensure(root))
+		return;
+
+	const auto slippage_speed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	const FVector correction_acceleration = -slippage_speed / DeltaTime * GetRightVector();
+	const FVector correction_force = (root->GetMass()) * correction_acceleration / 2.0f;
+
+	root->AddForce(correction_force);
+}
+
 void UTankTrackComponent::SetThrottle(float throttle)
 {
 	throttle = FMath::Clamp(throttle, -1.0f, 1.0f);
 
-	auto applied_force = GetForwardVector() * throttle * max_force * 100.0f;
-	auto force_location = GetComponentLocation();
-	auto root = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	const FVector applied_force = GetForwardVector() * throttle * max_force * 100.0f;
+	const FVector force_location = GetComponentLocation();
+	const auto root = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 
 	root->AddForceAtLocation(applied_force, force_location);
 }
--- Battle_Tanks/Source/Battle_Tanks/Public/TankTrackComponent.h
+++ Battle_Tanks/Source/Battle_Tanks/Public/TankTrackComponent.h
@@ -15,10 +15,19 @@ class BATTLE_TANKS_API UTankTrackComponent : public UStaticMeshComponent
 	GENERATED_BODY()
 
 public:
+
+	UTankTrackComponent();
+
+	void BeginPlay() override;
+	void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float throttle);
 
 	// Max force per track in Newton
 	UPROPERTY(EditDefaultsOnly)
 	float max_force = 700000.0f;
+
+private:
+	UStaticMeshComponent* root;
 };
